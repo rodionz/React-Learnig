a464fa07de3af94b554e4daba6ee47c4c162a4a1
Rodion Zinovyev <rodionz@012.net.il>
2018-02-16 20:18:46 +0200

    progress

--- lists-conditionals--assignment-problem/src/App.js
+++ lists-conditionals--assignment-problem/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation'
+import Charcomponent from './Charcomponent/Charcomponent'
 
 class App extends Component {
 
@@ -20,10 +21,10 @@ class App extends Component {
           <input onKeyUp={this.inputchange} type="text"/>
           <p>{this.state.count}</p>
            <Validation length={this.state.count}/>
-
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+           <Charcomponent/>
+         
+          
+          
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
---
+++ lists-conditionals--assignment-problem/src/Charcomponent/Charcomponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Charcomponent.css'
+
+const Charcomponent = (props) => {
+    return  (<div className="Charcomponent">Charcomponent</div>)   } 
+
+export default Charcomponent;
